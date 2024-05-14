8d011e80219c28b6dc110f7e3d3cc57b35bed4eb
liarcrown <liarcrown@naver.com>
2024-05-14 15:57:54 +0900

    feat: ex12

--- src/App.js
+++ src/App.js
@@ -1,11 +1,33 @@
-import React from "react";
+import React, { useRef } from "react";
 import InputSample from "./components/InputSample";
 import Counter from "./components/Counter";
 import InputSample2 from "./components/InputSample2";
 import UserList from "./components/UserList";
 
 function App() {
-  return <UserList></UserList>;
+  const users = [
+    {
+      id: 1,
+      username: "velopert",
+      email: "public.velopert@gmail.com",
+    },
+    {
+      id: 2,
+      username: "tester",
+      email: "tester@example.com",
+    },
+    {
+      id: 3,
+      username: "liz",
+      email: "liz@example.com",
+    },
+  ];
+  const nextId = useRef(4);
+  const onCreate = () => {
+    // 나중에 사용
+    nextId.current += 1;
+  };
+  return <UserList users={users} />;
 }
 
 export default App;
--- src/components/UserList.js
+++ src/components/UserList.js
@@ -1,27 +1,10 @@
 import React from "react";
 import User from "./user";
 
-function UserList() {
-  const users = [
-    {
-      id: 1,
-      username: "velopert",
-      email: "public.velopert@gmail.com",
-    },
-    {
-      id: 2,
-      username: "tester",
-      email: "tester@example.com",
-    },
-    {
-      id: 3,
-      username: "liz",
-      email: "liz@example.com",
-    },
-  ];
+function UserList(props) {
   return (
     <div>
-      {users.map((user) => (
+      {props.users.map((user) => (
         <User user={user} key={user.id}></User>
       ))}
     </div>
