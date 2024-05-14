a97eca15735178befda12833bd59740ca720346e
liarcrown <liarcrown@naver.com>
2024-05-14 14:51:37 +0900

    feat: add focus with useRef()

--- src/components/InputSample2.js
+++ src/components/InputSample2.js
@@ -1,23 +1,28 @@
-import React, { useState } from "react";
+import React, { useState, useRef } from "react";
 
-function InputSample2() {
-  const [inputText, setInputText] = useState({
+function InputSample() {
+  const [inputs, setInputs] = useState({
     name: "",
     nickname: "",
   });
+  const nameInput = useRef();
 
-  const { name, nickname } = inputText;
+  const { name, nickname } = inputs; // 비구조화 할당을 통해 값 추출
 
   const onChange = (e) => {
-    const { value, name } = e.target;
-    console.log(e.target); // <input placeholder = "이름">
-    setInputText({ ...inputText, [name]: value });
+    const { value, name } = e.target; // 우선 e.target 에서 name 과 value 를 추출
+    setInputs({
+      ...inputs, // 기존의 input 객체를 복사한 뒤
+      [name]: value, // name 키를 가진 값을 value 로 설정
+    });
   };
+
   const onReset = () => {
-    setInputText({
+    setInputs({
       name: "",
       nickname: "",
     });
+    nameInput.current.focus();
   };
 
   return (
@@ -27,26 +32,21 @@ function InputSample2() {
         placeholder="이름"
         onChange={onChange}
         value={name}
-      ></input>
+        ref={nameInput}
+      />
       <input
         name="nickname"
         placeholder="닉네임"
         onChange={onChange}
         value={nickname}
-      ></input>
+      />
       <button onClick={onReset}>초기화</button>
       <div>
-        <b>값 : </b>
-        {name} : ({nickname})
+        <b>값: </b>
+        {name} ({nickname})
       </div>
     </div>
   );
 }
 
-export default InputSample2;
-
-// export default function () {
-//   return (
-//     <div></div>
-//   )
-// }
+export default InputSample;
